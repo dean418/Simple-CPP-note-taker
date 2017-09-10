0bb3fe910caaaf6f2b4073b39b4a27760111cb4a
dean808 <deansingleton1999@gmail.com>
2017-09-10 01:59:34 +0100

    very hacky but the deletion of notes works

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,13 @@
 #include <cstring>
 #include <string>
 #include <cstdio>
+#include <vector>
+#include <cstddef>
+
 
 using namespace std;
+
+const int line = 50;
 
 int main () {
 	
@@ -14,16 +19,17 @@ int main () {
 	string selection; //variable to store a, b or c depending on users input 
 	string Read;
 	
-	cout << "C++ note taking.\n" << "Would you like to: (Type a, b, c or d to choose an option)\n\n" << "a) Make a new file.\nb) List all files.\nc) View a file.\nd) delete a file." << endl; 
+	cout << "C++ note taking.\n" << "Would you like to: (Type a, b, c or d to choose an option)\n\n" << "a) Make a new file.\nb) List all files.\nc) View a file.\nd) Delete a file." << endl; 
 	cin >> selection; // storing the users input in the "selection" variable
 
 	while (selection != "a" && selection != "b" && selection != "c" && selection != "d") { // loop until the user enters a valid letter
 		cout << "Please enter a, b or c\n";
 		cin >> selection;
-	}
+	} //OK
 	
+//---------- Make a new file -----------
 	
-	if (selection == "a") { // make a new file
+	if (selection == "a") {
 		
 		string file; //variable to store the file name
 		char text[512]; // variable to store the contents of te file
@@ -43,15 +49,16 @@ int main () {
 		cout << "\nThe name of your file is: " << file << endl;
 		cout << "The contents of your file are: " << text << endl;
 		
-		ofstream _titles_;                            //|
-		_titles_.open ("_titles.txt_", fstream::app); //|---every time a file is created add the title to a seperate file
-		_titles_ << file << ".txt" << "\n";           //|
-		_titles_.close();                             //|
+		ofstream _titles_; // object for the list of the names of notes
+		_titles_.open ("_titles_", fstream::app); //open and append the file named "_titles_";
+		_titles_ << file << "\n"; // write the name of the note that the user created to the file
+		_titles_.close(); // close the "_titles_" file
 		
+//----------- List all files ------------
 	
-	} else if (selection == "b") { // list all files
+	} else if (selection == "b") { //OK
 	
-		ifstream readFromFile("_titles.txt_"); //open the file _titles_ 
+		ifstream readFromFile("_titles_"); //open the file _titles_ 
 		
 		if (readFromFile.is_open()) { // if _titles_ is open do
 			
@@ -65,8 +72,10 @@ int main () {
 		} else { //error
 			cout << "you may no have permission to access these files or a different error has occured!" << endl;
 		}
-	
-	} else if (selection == "c") { // view a file
+		
+//------------ View a file ----------------
+
+	} else if (selection == "c") { //OK
 	
 		string fileToView; // variable to store the name of the file
 		
@@ -87,33 +96,75 @@ int main () {
 		} else {
 			cout << "The file you are trying to view does not exist!" << endl; //error
 		}
-	} else if (selection == "d") {
 		
-		string fileToDelete;
+//-------------- Delete a file ----------------
+
+	} else if (selection == "d") { //O
+		
+		string fileToDelete; // variable to store the name of the file that the user wants to delete 
 		
 		cout << "Enter the name of the file that you would like to delete." << endl;
-		cin >> fileToDelete;
+		cin >> fileToDelete; 
 		
 		ifstream Delete(fileToDelete);
 		
-		if (Delete.is_open()) {
+		if (Delete.is_open()) { //bad way of checking if a file exists
 			Delete.close();
 			
-			if(remove(fileToDelete.c_str()) != 0) {
-				cout << "file deleted" << endl;
+			if(remove(fileToDelete.c_str()) != 0) { // why does this only work inside the if statement
+			
+				cout << "file could not be deleted" << endl;
+			} else {
+				
+				cout << "Deleted the file: " << fileToDelete << endl;
 			}
 		}
+		//----------- deleting the file name from _titles_ -----------
+		
+		size_t found = Read.find_first_of(fileToDelete);
+		
+		ifstream titles("_titles_");
+			
+		if(titles.is_open()) {
+				
+			getline(titles, Read);
+			cout << Read << endl;
+			
+			while (found !=string::npos) {
+				
+				Read[found] = ' ';
+				found = Read.find_first_of(fileToDelete, found + 1);
+			}
+			
+			cout << Read << endl;
+			titles.close();
+		}
+		
+		remove("_titles_");
+			
+	
+	
+		ofstream deleted; // object for the list of the names of notes
+		deleted.open ("_titles_", fstream::app); //open and append the file named "_titles_";
+		deleted << Read << "\n"; // write the name of the note that the user created to the file
+		deleted.close(); // close the "_titles_" file
 	}
+		
+
+	
+//------------ end of delete from file -------------
+
 		string answer;
 		cout << "Would you like to chose a different option from the menu? (y/n)" << endl;
 		cin >> answer;
 		
-		if (answer == "y") {
-			
+		if (answer == "y") { // giving the user the option to re-run the program
+		                     // to chose a different option in the menu
 			repeat = true;
 		} else {
 			repeat = false;
 		}
 	}
-	return 0;
-}
+		return 0; //exit the program
+} // END INT MAIN
+	
